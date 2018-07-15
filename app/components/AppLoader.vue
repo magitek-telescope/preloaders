9a2f43db8fc6768195c7857b4fcb0a6d523d9642
HANATANI Takuma <mail@potato4d.me>
2018-07-15 16:21:03 +0900

    Fix component styles

--- app/components/AppLoader.vue
+++ app/components/AppLoader.vue
@@ -28,4 +28,46 @@ export default {
 
 <style scoped>
 
+.card {
+  width: 400px;
+  max-width: 100%;
+  border-radius: 2px;
+  overflow: hidden;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.093);
+  margin-bottom: 30px;
+}
+
+.card iframe {
+  width: 100%;
+  height: 300px;
+  border: 0;
+  overflow: hidden;
+}
+
+.card .card-title {
+  color: #666;
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 15px;
+}
+
+.card .card-show{
+  font-size: 13px;
+  color: #77CFF7;
+  cursor: pointer;
+}
+
+.card .card-show:hover{
+  text-decoration: underline;
+}
+
+@media (max-width: 600px) {
+  .card iframe {
+    width: calc(100vw - 20px);
+    height: calc(75vw);
+  }
+}
+
 </style>
